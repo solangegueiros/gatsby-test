fe240152913b9a9e3e7a74aca3757a0757e255c4
Solange Gueiros <solangegueiros@gmail.com>
2022-01-22 20:15:20 -0300

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
     siteMetadata: {
+        title: "Gatsby tutorial",
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
     ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Tigrinho na neve no Olimpic Park em Salt Lake City."
+        src="../images/SaltLakeCity-OlimpicPark-DSC00284.jpg"
+      />
+      <br/>
+      <br/>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />      
     </Layout>
   )
 }
 
 export default IndexPage
+
